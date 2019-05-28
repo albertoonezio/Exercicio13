e83a48f8770bf3ac282d24fe38b956cc0b2ed952
Alberto <albertomaronezio@hotmai.com>
2019-05-28 17:06:17 -0300

    Exercicio13

---
+++ Exercicio13/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double saldo = 0;
+            double reajuste = 0;
+            double juro = 0;
+            Console.Write("Digite o saldo da Aplicação: ");
+            saldo = double.Parse(Console.ReadLine());
+            Console.Write("Digite o Juro: ");
+            juro = double.Parse(Console.ReadLine());
+            reajuste = saldo * (juro / 100);
+            saldo = saldo + reajuste;
+            Console.WriteLine("Seu novo saldo é {0:#.00}", saldo);
+        }
+    }
+}
